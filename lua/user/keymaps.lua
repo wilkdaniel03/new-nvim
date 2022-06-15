197726a3eb32a868bd8287ee2b8f9766630abec5
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-15 17:54:04 +0200

    Adding nvim tree.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -59,3 +59,6 @@ keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- NvimTree
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
